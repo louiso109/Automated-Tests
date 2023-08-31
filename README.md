89adcc77c56442900a30a8e748c75c2445fd302f
louiso109 <louis.odinot@aspeq.com>
2023-09-01 09:55:03 +1200

    First automated test using before each (no page object used)

---
+++ README.md
@@ -0,0 +1,1 @@
+I have now been read
---
+++ cypress/integration/Automated-Test-BeforeEach.js
@@ -0,0 +1,11 @@
+//<reference types = "cypress" />
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/commands/actions')
+        })
+
+    it('should be able to add a new item to the list', () => {
+
+        cy.get('#email1').click()
+
+        .type('test.automation@email.com').should ('have.value', 'test.automation@email.com')
+    })

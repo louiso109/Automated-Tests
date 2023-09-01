55a09c64dc0712b6c2967777f12ac55808b59a2b
louiso109 <louis.odinot@aspeq.com>
2023-09-01 16:19:03 +1200

    Text exceptions file

---
+++ cypress/integration/Test-Exceptions.js
@@ -0,0 +1,9 @@
+// Test case 1 no such element exception
+
+// Test case 2 element not able to be interacted with
+
+// Test case 3 invalid element state exception
+
+// Test case 4 stle element reference exception
+
+// Test case 5 timeout exception
